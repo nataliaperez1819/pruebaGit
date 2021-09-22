7be8ffd0e943882af520f3154acbe6cf4e9be923
Natalia Pérez <nperezparejo@gmail.com>
2021-09-22 17:31:11 +0200

    Solución

--- BaseProgramacion_Ej12/src/paquete/Ejecuta.java
+++ BaseProgramacion_Ej12/src/paquete/Ejecuta.java
@@ -11,34 +11,35 @@ public class Ejecuta {
 			número es primo si tiene dos únicos divisores que son el 1 y él mismo.
 		*/
 		
-		int numero;
 		int resto;
 		boolean primo;
-		String mensaje;
-		Scanner sc = new Scanner(System.in);
+		//String mensaje;
+		//Scanner sc = new Scanner(System.in);
 		
-		//7
-		//posibles divisores son: 2, 3, 4, 5, 6
-		// 7%2
+		/*System.out.println("Introduzca numero");
+		numero = sc.nextInt();*/
 		
-		System.out.println("Introduzca numero");
-		numero = sc.nextInt();
+		//Debemos probar con los número comprendidos entre 1 y 100
 		
-		if (numero < 2) {
-			primo = false;
-		} else {
+		for (int numero = 1; numero <= 100; numero++) {
 			primo = true;
 			
+			if (numero < 2) {
+				primo = false;
+			}
 			for (int candidatoDivisor = 2; candidatoDivisor < numero && primo; candidatoDivisor++) {
 				resto = numero%candidatoDivisor;
 				
 				if (resto == 0) primo = false;
 			}
+			if (primo) {
+				System.out.println(numero);
+			}
 		}
 		//if abreviado
-		mensaje = (primo)?"Es primo":"No es primo";
-		System.out.println(mensaje);
+		//mensaje = (primo)?"Es primo":"No es primo";
+		//System.out.println(mensaje);
 		
-		sc.close();
+		//sc.close();
 	}
 }
