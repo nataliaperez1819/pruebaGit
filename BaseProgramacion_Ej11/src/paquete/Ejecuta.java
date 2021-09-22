2966a61cd5dd7bb10b0b2182529eda5fbf8b9c43
Natalia Pérez <nperezparejo@gmail.com>
2021-09-22 11:36:07 +0200

    NATALIA 22-09-2021 11:35 BaseProgramacion_Ej11 Solución

--- BaseProgramacion_Ej11/src/paquete/Ejecuta.java
+++ BaseProgramacion_Ej11/src/paquete/Ejecuta.java
@@ -1,5 +1,8 @@
 package paquete;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 public class Ejecuta {
 
 	public static void main(String[] args) {
@@ -11,12 +14,67 @@ public class Ejecuta {
 
 		 */
 		
-		/*String nombre;
-		int año;
-		int edad;
-		float mediaEdad;*/
+		/*int numero;
+		String apellido;
+		Scanner sc = new Scanner (System.in);
 		
+		System.out.println("Introduzca numero");
+		//numero = sc.nextInt(); //ERROR
+		//numero = Integer.parseInt(sc.nextLine());
+		numero = Integer.valueOf(sc.nextLine()); //Cuando Scanner tenga que capturar números y texto, pasar el tipo primitivo (número) a String para que no salte ningun problema
+		
+		System.out.println("Introduzca apellido");
+		apellido = sc.nextLine();
+		
+		System.out.println("Fin");*/
+		
+		String nombre;
+		String nombreMenor = "";
+		String mensaje;
+		int anoNacimiento;
+		int edad; 
+		int contEdad = 0; //Contador de edades
+		int suma = 0; //Acumulador de edades
+		float media;
+		int menor = Integer.MAX_VALUE;
+		int anoAhora = Calendar.getInstance().get(Calendar.YEAR);
+		Scanner sc = new Scanner(System.in);
+		
+		boolean continuar = true;
+		
+		do {
+			System.out.println("Introduzca nombre");
+			nombre = sc.nextLine();
+			
+			if (nombre.equals("fin")) {
+				continuar = false;
 
+			} else {
+				System.out.println("Introduzca año de nacimiento");
+				anoNacimiento = Integer.valueOf(sc.nextLine());
+				contEdad++;
+				
+				edad = anoAhora - anoNacimiento;
+				suma += edad;
+				
+				//edad | menor
+				//-------------
+				// 30  2147483647
+				// 20     30
+				// 20     20
+				if (edad < menor) {
+					menor = edad;
+					nombreMenor = nombre;
+				}
+			}
+		} while (continuar);
+		sc.close();
+		
+		if (contEdad > 0) {
+			media =  (float) suma / contEdad;
+			mensaje = "El más joven se llama " + nombreMenor + " y la media de edad es " + media;
+			System.out.println(mensaje);
+		}
+		
 	}
-
 }
