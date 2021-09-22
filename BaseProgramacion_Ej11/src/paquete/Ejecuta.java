c11c6eea12e23e60f4ebc19ea8ae5ae21ab35b51
nataliaperez1819 <55694425+nataliaperez1819@users.noreply.github.com>
2021-09-22 12:58:30 +0200

    Update Ejecuta.java

--- BaseProgramacion_Ej11/src/paquete/Ejecuta.java
+++ BaseProgramacion_Ej11/src/paquete/Ejecuta.java
@@ -7,10 +7,10 @@ public class Ejecuta {
 
 	public static void main(String[] args) {
 		/*
-		 	Programa que lee nombre y aÒo de nacimiento de una serie de alumnos hasta introducir
-			como nombre de alumno ìfinî. En ese momento mostrar· la edad media de los alumnos y
-			el nombre del alumno menor. NOTA: Al introducir como nombre ìfinî, no nos ha de pedir el
-			aÒo de nacimiento.
+		 	Programa que lee nombre y a√±o de nacimiento de una serie de alumnos hasta introducir
+			como nombre de alumno ‚Äúfin‚Äù. En ese momento mostrar√° la edad media de los alumnos y
+			el nombre del alumno menor. NOTA: Al introducir como nombre ‚Äúfin‚Äù, no nos ha de pedir el
+			a√±o de nacimiento.
 
 		 */
 		
@@ -21,7 +21,7 @@ public class Ejecuta {
 		System.out.println("Introduzca numero");
 		//numero = sc.nextInt(); //ERROR
 		//numero = Integer.parseInt(sc.nextLine());
-		numero = Integer.valueOf(sc.nextLine()); //Cuando Scanner tenga que capturar n˙meros y texto, pasar el tipo primitivo (n˙mero) a String para que no salte ningun problema
+		numero = Integer.valueOf(sc.nextLine()); //Cuando Scanner tenga que capturar n√∫meros y texto, pasar el tipo primitivo (n√∫mero) a String para que no salte ningun problema
 		
 		System.out.println("Introduzca apellido");
 		apellido = sc.nextLine();
@@ -43,7 +43,7 @@ public class Ejecuta {
 		
 		//nombre  |  nombreMenor  |  anoNacimiento  |  edad  |  contEdad  |  suma  |  media  |  menor  |  anoAhora  |  mensaje
 		//======================================================================================================================
-		// MarÌa        ""/null          1997           24	      0           0               2147483647    2021    
+		// Mar√≠a        ""/null          1997           24	      0           0               2147483647    2021    
 		// Pedro        Pedro            2000           21        1        00+24=>24             21       
 		// fin													  2		   24+21=>45   22.5
 		
@@ -51,7 +51,7 @@ public class Ejecuta {
 		nombre = sc.nextLine();
 		
 		while (nombre.equals("fin") == false) {
-			System.out.println("Introduzca aÒo de nacimiento");
+			System.out.println("Introduzca a√±o de nacimiento");
 				anoNacimiento = Integer.valueOf(sc.nextLine());
 				
 				contEdad++;
@@ -73,12 +73,14 @@ public class Ejecuta {
 		}
  		sc.close();
 		
- 		//Si hemos llegado a pedir alg˙n nombre, la variable contEdad tendr· un valor > 0
+ 		//Si hemos llegado a pedir alg√∫n nombre, la variable contEdad tendr√° un valor > 0
 		if (contEdad > 0) {
 			media =  (float) suma / contEdad;
-			mensaje = "El m·s joven se llama " + nombreMenor + " y la media de edad es " + media;
+			mensaje = "El m√°s joven se llama " + nombreMenor + " y la media de edad es " + media;
 			System.out.println(mensaje);
 		}
 		
 	}
+	
+	//fin
 }
