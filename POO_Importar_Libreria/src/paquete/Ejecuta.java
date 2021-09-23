e30e68758fc9eb3c4af97b179e11cdfdda79d0ae
Natalia Pérez <nperezparejo@gmail.com>
2021-09-23 14:12:34 +0200

    Exportar e importar JAR e incorporarlo a la libreria de un proyecto OK

---
+++ POO_Importar_Libreria/src/paquete/Ejecuta.java
@@ -0,0 +1,12 @@
+package paquete;
+
+import clases.Vacaciones;
+
+public class Ejecuta {
+
+	public static void main(String[] args) {
+		Vacaciones v = new Vacaciones(15, "Roma", true);
+		v.mostrar();
+	}
+
+}
