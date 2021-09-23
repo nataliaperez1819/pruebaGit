72a11e1a68228633bfd41c04dd623a5a7014697d
Natalia Pérez <nperezparejo@gmail.com>
2021-09-23 13:12:45 +0200

    Creación de constructores de la clase Vacaciones

--- POO_Modificadores_Acceso/src/paquete/Vacaciones.java
+++ POO_Modificadores_Acceso/src/paquete/Vacaciones.java
@@ -6,4 +6,18 @@ public class Vacaciones {
 	public int dias;
 	public String destino;
 	public boolean playa;
+	
+	//Constructores
+	public Vacaciones(int dias, String destino, boolean playa) {
+		this.dias = dias;
+		this.destino = destino;
+		this.playa = playa;
+	}
+	
+	public Vacaciones() {
+	}
+	
+	
+	
+	
 }
