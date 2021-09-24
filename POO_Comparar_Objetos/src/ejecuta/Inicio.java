79956949df65af92161c34ffecdbf2d6bc3f15c7
Natalia Pérez <nperezparejo@gmail.com>
2021-09-24 11:43:25 +0200

    Última información explicación

--- POO_Comparar_Objetos/src/ejecuta/Inicio.java
+++ POO_Comparar_Objetos/src/ejecuta/Inicio.java
@@ -6,9 +6,17 @@ public class Inicio {
 
 	public static void main(String[] args) {
 
+		//Comprobar si el número 10 se encuentra en el siguiente array
+		//-----------
+		//|7|9|10|5|8|
+		//-----------
+		// 0 1 2  3 4
+		//i=========>
+		
 		Movil movilJuan = new Movil(1, 4, "Xiaomi", 3);
 		Movil movilMaria = new Movil(2, 4, "Xiaomi", 3);
 
+		//Comparar dos objetos
 		if (movilJuan.getImei() == movilMaria.getImei()) {
 			System.out.println("Iguales");
 		} else {
