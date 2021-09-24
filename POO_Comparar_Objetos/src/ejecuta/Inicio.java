e5cda01dcfac93df6986dbf6f781b82503e7656b
Natalia Pérez <nperezparejo@gmail.com>
2021-09-24 10:56:21 +0200

    Primer commit practicando la comparación de objetos

---
+++ POO_Comparar_Objetos/src/ejecuta/Inicio.java
@@ -0,0 +1,17 @@
+package ejecuta;
+
+import clases.Movil;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+
+		Movil movilJuan = new Movil(4, "Xiaomi", 3);
+		Movil movilMaria = new Movil(6, "Apple", 3);
+
+		if (movilJuan.equals(movilMaria)) {
+			
+		}
+	}
+
+}
